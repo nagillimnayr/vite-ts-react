0a1f846924a1d4f1c48f45b138b671598e22611c
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-06 19:46:04 -0700

    fix: correct path to 'index.tsx'

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from './App';
 const main = ()=>{
   const rootElement = document.createElement('main');
   document.body.appendChild(rootElement);
@@ -8,7 +9,7 @@ const main = ()=>{
   );
   root.render(
     <React.StrictMode>
-      <App />
+      <App>Hello World</App>
     </React.StrictMode>
   );
 }
