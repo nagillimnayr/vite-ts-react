15ca0d256f95dcbcd28acab61d8f9804f52af81f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-29 20:39:03 -0700

    build: install airbnb-typescript config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,10 @@
 module.exports = {
     extends: [
         'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
         'plugin:import/recommended',
         'eslint-config-airbnb',
+        'eslint-config-airbnb-typescript',
         'prettier',
     ],
     settings: {
@@ -15,6 +17,11 @@ module.exports = {
             },
         },
     },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
     env: {
         browser: true,
         node: true,
